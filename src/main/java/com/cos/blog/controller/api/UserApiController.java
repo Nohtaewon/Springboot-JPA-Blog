82ab9061e0613929eb8ecd19d4e26642c3c6d2c7
Nohtaewon <gkgk713@naver.com>
2022-08-03 00:57:03 +0900

    ~52 security login

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,7 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-
-	@PostMapping("/api/user")
+	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		// username, password, email
 		System.out.println("UserApiController:save 호출됨");
@@ -30,7 +30,27 @@ public class UserApiController {
 		// 자바오브젝트를 JSON으로 변환해서 리턴(jackson)
 	}
 	
-	@PostMapping("/api/user/login")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* 전통적인 방식
+	 * @PostMapping("/api/user/login")
 	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session){
 		System.out.println("UserApiController:login 호출됨");  
 		User principal= userService.로그인(user); //principal(접근주체) 
@@ -39,5 +59,6 @@ public class UserApiController {
 			session.setAttribute("principal", principal);
 		}
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-	}
+	}*/
+	
 }
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,15 +12,21 @@ import com.cos.blog.model.User;
 // 자동으로 bean 등록이 된다.
 // @Repository 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer>{ 
-	// 해당 JpaRepository 는 User 테이블이 관리하는 Repository 야
-	// 그리고 user 테이블의 pk는 Integer이야
 
-	// JPA Naming 전략
-	// select * from user where username=? and password=?;
-	User findByUsernameAndPassword(String username, String password);
-	
-//	@Query(value="select * from user where username=?1 and password=?2", nativeQuery = true)
-//	User login(String username, String password);
+	// select * from user where username = 1?;
+	Optional<User> findByUsername(String username);
 	
 	
 }
+
+
+
+// 해당 JpaRepository 는 User 테이블이 관리하는 Repository 야
+// 그리고 user 테이블의 pk는 Integer이야
+
+// JPA Naming 전략
+// select * from user where username=? and password=?;
+//User findByUsernameAndPassword(String username, String password);
+
+//	@Query(value="select * from user where username=?1 and password=?2", nativeQuery = true)
+//	User login(String username, String password);
